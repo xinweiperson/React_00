af947e299c5f2111c19c4fd36c78929f0d52c278
DESKTOP-ECK5JCH\Administrator <xinweiperson@gmail.com>
2025-09-14 21:02:52 +0800

    prors in react components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
-import image from './logo192.png'
-//flexible to fill properties
-function App() {
-
-  // const imgStyleObj = {
-  //         width: 200,
-  //         height: 200,
-  //         backgroundColor: 'grey'
-  //       }
-
-  // return (
-  //   <div>
-  //     <img
-  //     src={image}
-  //     alt="aa" 
-  //     className="small"
-  //     style={
-  //       {
-  //         imgStyleObj
-  //       }
-  //     }
-  //     />
-  //   </div>
-  // )
-
-    const imgData = {
-      className: "small",
-      stype: {
-         width: 200,
-          height: 200,
-          backgroundColor: 'grey'
-      }
-  }
-
+//prors in react components 
+function Article({title, content, active}){
   return (
     <div>
-      <img
-        src={image}
-        alt="hey"
-        {...imgData}// react grammer not es6.  {} is react mark, react do something when loading this || one point of interview
-        //https://www.bilibili.com/video/BV1xM41197cZ?spm_id_from=333.788.videopod.sections&vd_source=0639e655c5231e8d5460752b266e84f2
-      />
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <p>status: {active ? 'displaying' : 'disapear'}</p>
     </div>
   )
+}
+
+function App() {
+  return (
+    <>
+      <Article title="title1" content="content1" active/>
+      <Article title="title2" content="content2"/>
+      <Article title="title3" content="content3"/>
+    </>
+    )
 }
 
 export default App;
